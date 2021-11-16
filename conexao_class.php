fe2c0342e29a585b29d406785f71e36376d859b2
Vigorcosta <87789002+Vigorcosta@users.noreply.github.com>
2021-11-16 14:17:43 -0300

    Add files via upload

---
+++ conexao_class.php
@@ -0,0 +1,43 @@
+<?php
+
+class conexao {
+	
+	public $servidor = 'localhost';
+	public $usuario  = 'root';
+	public $senha    = '';
+	public $banco    = 'ensino';
+	public $porta    = '3306';
+	public $conexao;
+		
+	function conectar() {
+		
+			# Sem Porta
+			$conexao = mysqli_connect(
+			$this->servidor,
+			$this->usuario,
+			$this->senha,
+			$this->banco )
+			or die('Falha de conexão!');
+						
+			# Com Porta
+			# $conexao = mysqli_connect($this->servidor,
+			# $this->usuario,
+			# $this->senha,
+			# $this->banco,
+			# $this->porta)
+			# or die('Falha de conexão!');
+			
+			echo "Conexão foi realizada com sucesso!";
+			return $conexao;
+		}
+
+		
+}
+
+$obj = new conexao();
+$obj->conectar();
+
+
+
+
+?>
